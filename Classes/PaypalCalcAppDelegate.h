8f33931e77537612c2bfebc08eb895ab0b7d41cc
Sue Brown <crunchy@houseofcrunchy.com>
2010-05-26 14:15:36 -0700

    first commit

---
+++ Classes/PaypalCalcAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  PaypalCalcAppDelegate.h
+//  PaypalCalc
+//
+//  Created by Sue Brown on 5/26/10.
+//  Copyright House of Crunchy 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface PaypalCalcAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PaypalCalc
+//
+//  Created by Sue Brown on 5/26/10.
+//  Copyright House of Crunchy 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"PaypalCalcAppDelegate");
+    [pool release];
+    return retVal;
+}
